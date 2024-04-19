c8154f69aaadf8557ec7e9801a9afaaf39464880
Julian Mayola <julianmayola131@gmail.com>
2024-04-19 14:38:42 -0300

    cambios en el rectangulo

--- rectangulop5/rectangulop5.js
+++ rectangulop5/rectangulop5.js
@@ -1,17 +1,22 @@
-let r = 0;
+let r = 255;
 let g = 0;
 let b = 0;
+let inicio = 0;
 function setup(){
   createCanvas(800,600);
 }
 
 function draw(){
-  background(240,240,255);
   noStroke();
-  rect(300,200, 200,125);
-  fill(r,g,b);
-  delay(2000);
+  inicio ++;
+    fill(r,g,b);
+    rect(300,200, 200,125);
+    if(inicio == 120){
+      inicio = 0;
+      
   r = int(random(0,255));
   g = int(random(0,255));
   b = int(random(0,255));
+
+    }
 }
