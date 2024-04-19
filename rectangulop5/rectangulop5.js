8ffb63007f490939deeb269c128f6a0d4c44d641
Julian Mayola <julianmayola131@gmail.com>
2024-04-19 14:47:39 -0300

    añadidos segundos en el rectangulo

--- rectangulop5/rectangulop5.js
+++ rectangulop5/rectangulop5.js
@@ -7,7 +7,12 @@ function setup(){
 }
 
 function draw(){
+  background(255,255,255);
   noStroke();
+    fill(0,0,0);
+
+  text(int(inicio)+ " frames", 300, 100);
+  text("60 frames es un segundo", 300, 50);
   inicio ++;
     fill(r,g,b);
     rect(300,200, 200,125);
@@ -17,6 +22,6 @@ function draw(){
   r = int(random(0,255));
   g = int(random(0,255));
   b = int(random(0,255));
-
+  
     }
 }
